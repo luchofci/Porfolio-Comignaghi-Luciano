77791f365d2e0fa02d83996adb9641bfcb3d98c7
luchofci <luciano.federico.c@gmail.com>
2024-04-22 13:22:18 -0300

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
+import { NavBar } from './components/NavBar/NavBar'
 
 function App() {
 
   return (
     <>
-     
+      <NavBar/>
+      <h1>Luciano Porfolio</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit illum earum, natus amet libero saepe vero fugit assumenda nam ea dignissimos accusantium veniam expedita, ullam doloribus sapiente nisi omnis est.
+      Facilis, modi? Doloribus similique aut velit voluptas dolor illo neque eveniet assumenda ipsam facere cum, officia non, possimus expedita quis nisi quisquam. Sed nam aut aperiam aspernatur quisquam rem tempore.
+      </p>
+
     </>
   )
 }
 export default App
-asd
