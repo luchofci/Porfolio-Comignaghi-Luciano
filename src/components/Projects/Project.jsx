5dfaf4313f46722bd1941b44676e1a56dba0ec15
luchofci <luciano.federico.c@gmail.com>
2024-05-08 12:27:01 -0300

    Fix css

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,60 +1,84 @@
-import './Project.css'
+import './Project.css';
 // import FotoTools from './../assets/images/Foto.png'
 
-
-
 const Project = () => {
-  return (
-    <>
-      <div className="porfolio">
+	return (
+		<>
+			<div className="porfolio">
+				<div className="container">
+					<h1 className="sub-title">My work</h1>
 
-<div className="container">
-    <h1 className="sub-title">My work</h1>
-    
-    <div className="work-list">
-        <div className="work">
-            <img src={'../../../public/images/Foto.png'} alt="Foto1"/>
-            <div className="layer">
-                <h3>Social Media App</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque magnam officiis quos deserunt, ratione autem iusto eos adipisci sunt inventore, quis explicabo exercitationem temporibus, reiciendis error! Eos quis quas magni.</p>
-                <a href="#"><i className="fa-solid fa-arrow-up-right-from-square"></i> </a>
-            </div>
+					<div className="work-list">
+						<div className="work">
+							<img
+								src={'../../../public/images/Foto.png'}
+								alt="Foto1"
+							/>
+							<div className="layer">
+								<h3>Social Media App</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetur
+									adipisicing elit. Neque magnam officiis quos
+									deserunt, ratione autem iusto eos adipisci
+									sunt inventore, quis explicabo
+									exercitationem temporibus, reiciendis error!
+									Eos quis quas magni.
+								</p>
+								<a href="#">
+									<i className="fa-solid fa-arrow-up-right-from-square"></i>{' '}
+								</a>
+							</div>
+						</div>
 
-        </div>
-        
-        <div className="work">
-            <img src={'../../../public/images/Foto.png'} alt="Foto2"/>
-            <div className="layer">
-                <h3>Social Media App</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque magnam officiis quos deserunt, ratione autem iusto eos adipisci sunt inventore, quis explicabo exercitationem temporibus, reiciendis error! Eos quis quas magni.</p>
-                <a href="#"><i className="fa-solid fa-arrow-up-right-from-square"></i> </a>
-            </div>
-        </div>
-        
-        <div className="work">
-            <img src={'../../../public/images/Foto.png'} alt="Foto3"/>
-            <div className="layer">
-                <h3>social Media App</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque magnam officiis quos deserunt, ratione autem iusto eos adipisci sunt inventore, quis explicabo exercitationem temporibus, reiciendis error! Eos quis quas magni.</p>
-                <a href="#"><i className="fa-solid fa-arrow-up-right-from-square"></i>
-                  </a>
-            </div>
-        </div>
-    </div>
-    <a href="" className="btn">See more</a>
-</div>
-</div>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    </>
-  )
-}
+						<div className="work">
+							<img
+								src={'../../../public/images/Foto.png'}
+								alt="Foto2"
+							/>
+							<div className="layer">
+								<h3>Social Media App</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetur
+									adipisicing elit. Neque magnam officiis quos
+									deserunt, ratione autem iusto eos adipisci
+									sunt inventore, quis explicabo
+									exercitationem temporibus, reiciendis error!
+									Eos quis quas magni.
+								</p>
+								<a href="#">
+									<i className="fa-solid fa-arrow-up-right-from-square"></i>{' '}
+								</a>
+							</div>
+						</div>
 
-export default Project
+						<div className="work">
+							<img
+								src={'../../../public/images/Foto.png'}
+								alt="Foto3"
+							/>
+							<div className="layer">
+								<h3>social Media App</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetur
+									adipisicing elit. Neque magnam officiis quos
+									deserunt, ratione autem iusto eos adipisci
+									sunt inventore, quis explicabo
+									exercitationem temporibus, reiciendis error!
+									Eos quis quas magni.
+								</p>
+								<a href="#">
+									<i className="fa-solid fa-arrow-up-right-from-square"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+					<a href="" className="btn">
+						See more
+					</a>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Project;
