4513974e4c3cccf9bd7f6f5936265a664aaadf46
luchofci <luciano.federico.c@gmail.com>
2024-05-08 08:55:40 -0300

    Add copywrith

---
+++ src/components/Footer/CopyRigth/CopyRigth.jsx
@@ -0,0 +1,12 @@
+import './CopyRigth.css'
+
+
+const CopyRigth = () => {
+    return (
+        <div className="copyright">
+        <p>Copyright <i className="fa-solid fa-copyright"></i> Luciano Comignaghi Dev // Making your dream comes code <i className="fa-solid fa-code"></i></p>
+    </div>
+    )
+}
+
+export default CopyRigth
