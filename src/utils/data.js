90bad531e38b315c7852f41d028fe10cea0b79bd
luchofci <luciano.federico.c@gmail.com>
2024-05-10 18:17:27 -0300

    fix skill info

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,60 +1,56 @@
-import FotoFrontEnd from './../../public/images/front/arte-digital.png'
-import FotoBackend from './../../public/images/back/back-end (2).png'
-import FotoTools from './../../public/images/tools/herramientas.png'
-import FotoSoftSkills from './../../public/images/soft/habilidades-blandas.png'
-
+import FotoFrontEnd from './../../public/images/front/arte-digital.png';
+import FotoBackend from './../../public/images/back/back-end (2).png';
+import FotoTools from './../../public/images/tools/herramientas.png';
+import FotoSoftSkills from './../../public/images/soft/habilidades-blandas.png';
 
 export const SKILLS = [
-    {
-        title:'FrontEnd',
-        icon: FotoFrontEnd,
-        skills:[
-            {skills:'HTML', percentage: "90%"},
-            {skills:'CSS', percentage: "90%"},
-            {skills:'JavaScript', percentage: "75%"},
-            {skills:'React.js', percentage: "90%"},
-            {skills:'Boostrap', percentage: "80%"},
-        ]
-    },
-    {
-        title:'BackEnd',
-        icon: FotoBackend,
-        skills:[
-            {skills:'Node.js', percentage: "70%"},
-            {skills:'Express.js', percentage: "70%"},
-            {skills:'MongooDB', percentage: "80%"},
-        ]
-    },
-    {
-        title:'Tools',
-        icon: FotoTools,
-        skills:[
-            {skills:'Visual Studio Code', percentage: "90%"},
-            {skills:'Git & GitHub', percentage: "85%"},
-            {skills:'Responsive Design', percentage: "75%"},
-            {skills:'Figma & FigJam', percentage: "95%"},
-        ]
-    },
-    {
-        title:'SoftSkills',
-        icon: FotoSoftSkills,
-        skills:[
-            {skills:'Team Player', percentage: "90%"},
-            {skills:'Work Under Pressure', percentage: "100%"},
-            {skills:' Problem Solving & Decision-Making', percentage: "85%"},
-            {skills:'Effective Communication', percentage: "95%"},
-            {skills:'Leadership & Mentorship', percentage: "80%"},
-        ]
-    },
-]
-
+	{
+		title: 'FrontEnd',
+		icon: FotoFrontEnd,
+		skills: [
+			{ skills: 'HTML', percentage: '90%' },
+			{ skills: 'CSS', percentage: '90%' },
+			{ skills: 'JavaScript', percentage: '75%' },
+			{ skills: 'React.js', percentage: '90%' },
+			{ skills: 'Boostrap', percentage: '80%' },
+		],
+	},
+	{
+		title: 'BackEnd',
+		icon: FotoBackend,
+		skills: [
+			{ skills: 'Node.js', percentage: '75%' },
+			{ skills: 'Express.js', percentage: '75%' },
+			{ skills: 'MongooDB', percentage: '80%' },
+		],
+	},
+	{
+		title: 'Tools',
+		icon: FotoTools,
+		skills: [
+			{ skills: 'Visual Studio Code', percentage: '90%' },
+			{ skills: 'Git & GitHub', percentage: '85%' },
+			{ skills: 'Responsive Design', percentage: '80%' },
+			{ skills: 'Figma & FigJam', percentage: '95%' },
+		],
+	},
+	{
+		title: 'SoftSkills',
+		icon: FotoSoftSkills,
+		skills: [
+			{ skills: 'Team Player', percentage: '100%' },
+			{ skills: 'Work Under Pressure', percentage: '100%' },
+			{ skills: ' Problem Solving & Decision-Making', percentage: '90%' },
+			{ skills: 'Effective Communication', percentage: '95%' },
+			{ skills: 'Leadership & Mentorship', percentage: '85%' },
+		],
+	},
+];
 
 export const WORK_EXPERIENCE = [
-    {
-        title: '',
-        date:"",
-        responsabilities:[
-            ""
-        ],
-    },
-]
+	{
+		title: '',
+		date: '',
+		responsabilities: [''],
+	},
+];
